96f4b540f5e23b7e8f3d5879257ba188672d5aa1
Camille <camille.coutens@gmail.com>
2021-07-03 16:07:53 +0200

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="home container">
+    <!-- Header -->
+    <div class="header flex">
+      <div class="left flex flex-column">
+        <h1>Invoices</h1>
+        <span>There are 4 total invoices</span>
+      </div>
+      <div class="right flex">
+        <div class="filter flex" @click="toggleFilterMenu">
+          <span>Filter by status</span>
+          <img src="@/assets/icon-arrow-down.svg" alt="" />
+          <ul v-show="filterMenu" class="filter-menu">
+            <li @click="filteredInvoices">Draft</li>
+            <li @click="filteredInvoices">Pending</li>
+            <li @click="filteredInvoices">Paid</li>
+            <li @click="filteredInvoices">Clear Filter</li>
+          </ul>
+        </div>
+        <div class="button flex" @click="newInvoice">
+          <div class="inner-button flex">
+            <img src="@/assets/icon-plus.svg" alt="" />
+          </div>
+          <span>New Invoice</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      filterMenu: null,
+    }
+  },
+  methods: {
+    newInvoice() {},
+
+    toggleFilterMenu() {
+      this.filterMenu = !this.filterMenu
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.home {
+  color: #fff;
+
+  .header {
+    margin-bottom: 65px;
+  }
+
+  .left, 
+  .right {
+    flex: 1
+  }
+
+  .right {
+    justify-content: flex-end;
+    align-items: center;
+
+    .button,
+    .filter {
+      align-items: center;
+
+      span {
+        font-size: 12px;
+      }
+    }
+
+    .filter {
+      position: relative;
+      margin-right: 40px;
+      cursor: pointer;
+
+      img {
+        margin-left: 12px;
+        width: 9px;
+        height: 5px;
+      }
+
+      .filter-menu {
+        width: 120px;
+        position: absolute;
+        top: 25px;
+        list-style: none;
+        background-color: #292929;
+        box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+        li {
+          cursor: pointer;
+          font-size: 12px;
+          padding: 10px 20px;
+          &:hover {
+            color: #292929;
+            background-color: #fff;
+          }
+        }
+      }
+    }
+
+    .button {
+      padding: 8px 10px;
+      background-color: #7c5dfa;
+      border-radius: 40px;
+      .inner-button {
+        margin-right: 8px;
+        border-radius: 50%;
+        padding: 8px;
+        align-items: center;
+        justify-content: center;
+        background-color: #fff;
+        img {
+          width: 10px;
+          height: 10px;
+        }
+      }
+    }
+  }
+}
+</style>
